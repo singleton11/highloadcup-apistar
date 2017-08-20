4d28681a99cfe8654f4b50462e03d807da5b004b
Anton Prokhorov <prokhorov@saritasa.com>
2017-08-20 21:06:50 +0700

    Make `users.gender` char field

--- migrations/initial.sql
+++ migrations/initial.sql
@@ -4,7 +4,7 @@ CREATE TABLE users
   email      VARCHAR(100)    NOT NULL,
   first_name VARCHAR(50)     NOT NULL,
   last_name  VARCHAR(50)     NOT NULL,
-  gender     BOOLEAN DEFAULT TRUE NOT NULL,
+  gender     VARCHAR(1) DEFAULT 'm' NOT NULL,
   birth_date INT             NOT NULL
 );
 CREATE UNIQUE INDEX users_email_uindex
